5584af0bfee23f9ccf9ac6310e204b0dd0c493f8
TkMwangi <51172995+TonnyKamau@users.noreply.github.com>
2024-04-04 13:43:36 +0300

    home

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:wallstreet/colours/colours.dart';
-
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,20 +20,62 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: SizedBox(
-                height: 100,
+                height: 70,
                 width: double.infinity,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton(
-                      onPressed: (){},
-                      icon: const Icon(Icons.menu),
-                      color: icons,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: GestureDetector(
+                            onTap: () => print("Tapped"),
+                            child: Row(
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/visa.svg',
+                                  width: 40,
+                                  height: 40,
+                                ),
+                                SvgPicture.asset(
+                                  'assets/mastercard.svg',
+                                  width: 40,
+                                  height: 40,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Material(
+                          color: Colors.transparent,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset(
+                              'assets/plus.svg',
+                              width: 40,
+                              height: 40,
+                            ),
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors
+                                .transparent, // Additionally set highlight color to transparent
+                            hoverColor: Colors
+                                .transparent, // Set hover color to transparent if needed
+                          ),
+                        ),
+                      ],
                     ),
                     IconButton(
                       onPressed: () {},
-                      icon: SvgPicture.asset('assets/menu.svg'),
-                      color: icons,
+                      icon: SvgPicture.asset(
+                        'assets/menu.svg',
+                        width: 40,
+                        height: 40,
+                      ),
+                      color: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
                     ),
                   ],
                 ),
